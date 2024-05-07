a981c70b755fe5e6a60e25bf747c48f197e938f7
qkrwnsduda1 <qkrwnsduda1@naver.com>
2024-05-07 23:02:33 +0900

    Helper 기능 추가
    txt 데이터 파일 불러오기, txt 파일 백업

--- LMS/src/main/java/cseProject/SystemHelper.java
+++ LMS/src/main/java/cseProject/SystemHelper.java
@@ -1,8 +1,17 @@
 package cseProject;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
@@ -33,4 +42,94 @@ public class SystemHelper {
         String input = br.readLine();
         return input;
     }
+    
+    
+      
+            public static ArrayList<String[]> getTextedData(String parameter){    //파일에 저장된 데이터를 리스트에 읽어드림
+                
+                ArrayList<String[]> list = new ArrayList<>();
+                
+            try{           
+                File f = new File(parameter);
+        if(f.exists()){
+        }else if(f.createNewFile()){ 
+        }
+                
+        FileInputStream input=new FileInputStream(parameter);
+        InputStreamReader reader=new InputStreamReader(input,"UTF-8");
+        BufferedReader in=new BufferedReader(reader);
+  
+         
+            String str0="";
+            while(true)
+            {
+                if(str0.contains("*")){ //*는 데이터의 끝을 나타냄
+                str0 = str0.substring(0, str0.length() - 1); //끝에 붙은 * 제거
+                break;
+                }
+                str0 += in.readLine();   
+            }
+
+
+            
+           // System.out.println(str0);
+            
+            String[] str1 = str0.split("/"); 
+            for(String temp:str1){
+                list.add(temp.split(",")); 
+                //System.out.println(temp);
+            }
+            
+            for(String[] temp:list){
+                for(String str:temp){
+                    //System.out.println(str);
+                }
+            }
+           
+             
+            in.close();
+         
+                 return list;
+        } catch (NullPointerException | IOException e){
+            e.getStackTrace();
+             String[] str = new String[0]; 
+            return list; 
+        }   
+    }
+        
+   
+    public static void BackUpTextFile(String paramater){   
+        
+         LocalDateTime now = LocalDateTime.now(); 
+         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");  
+         String formatedNow = now.format(formatter);
+        
+           File folder = new File( paramater+"_old");
+
+           if (!folder.exists()) {
+		try{
+		    folder.mkdir();
+	        } 
+	        catch(Exception e){
+		    e.getStackTrace();
+		}             
+         }
+         
+           String patch = paramater+"_old/"+formatedNow+".txt";
+          
+           File file = new File( paramater);
+           File newFile = new File(patch);
+           
+        try {    
+            Files.copy(file.toPath(), newFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException ex) {
+            Logger.getLogger(SystemHelper.class.getName()).log(Level.SEVERE, null, ex);
+        }
+           
+            
+                file.delete();          
+    }
+        
+
+    
 }
