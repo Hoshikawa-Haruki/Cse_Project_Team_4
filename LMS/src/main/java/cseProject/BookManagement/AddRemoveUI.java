99b1ef20d268746bfea4e0ddcdc26c8a340ff475
Soeun0 <144868086+Soeun0@users.noreply.github.com>
2024-05-19 14:22:40 +0900

    입력 간단하게 수정

--- LMS/src/main/java/cseProject/BookManagement/AddRemoveUI.java
+++ LMS/src/main/java/cseProject/BookManagement/AddRemoveUI.java
@@ -56,101 +56,116 @@ public class AddRemoveUI {
     }
 
     private void add(BookList books) {
-        try (Scanner scanner = new Scanner(System.in)) {
-            System.out.println("<책 정보 등록시 주의사항>");
-            System.out.println("1. 책 제목, 저자, 출판사, ISBN 순서로 입력한다.");
-            System.out.println("2. 각 정보마다 TAB키로 구분해준다.\n");
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
 
-            while (true) {
-                String line = null;
+            String title;
+            String author;
+            String publisher;
+            String isbn;
 
-                System.out.print("▶ 책 정보를 입력하시오: ");
-                line = scanner.nextLine();
-                String[] tokens = line.split("\t");
+            while(true) {
+                System.out.print("▶ 책 제목을 입력하세요: ");
+                title = br.readLine();
 
-                if (tokens.length != 4) {
-                    System.out.println("입력이 잘못되었습니다. 다시 입력해주세요.");
+                System.out.print("▶ 저자를 입력하세요: ");
+                author = br.readLine();
+
+                System.out.print("▶ 출판사를 입력하세요: ");
+                publisher = br.readLine();
+
+                System.out.print("▶ ISBN을 입력하세요: ");
+                isbn = br.readLine();
+
+                if(title.isEmpty() || author.isEmpty() || publisher.isEmpty() || isbn.isEmpty()){
+                    System.out.println("다시 입력하시기 바랍니다.");
                     continue;
-                } else {
-                    Book book = new Book(tokens[0], tokens[1], tokens[2], tokens[3]);
-                    System.out.println("제목: " + tokens[0]);
-                    System.out.println("저자: " + tokens[1]);
-                    System.out.println("출판사: " + tokens[2]);
-                    System.out.println("ISBN: " + tokens[3]);
-
-                    while (true) {
-                        System.out.print("▶ 해당 내용이 맞습니까? (y/n) : ");
-                        String Check = scanner.nextLine();
-
-                        switch (Check) {
-                            case "y":
-                                books.addBook(book);
-                                System.out.println("책이 성공적으로 추가되었습니다.");
-                                System.out.println("------------------------------------");
-                                return;
-                            case "n":
-                                System.out.println("취소되었습니다.");
-                                System.out.println("------------------------------------");
-                                return;
-                            default:
-                                System.out.println("다시 입력해주세요.");
-                                break;
-                        }
-                    }
+                }
+                else {
+                    break;
                 }
             }
+
+
+            Book book = new Book(title, author, publisher, isbn);
+
+            System.out.println("제목: " + title);
+            System.out.println("저자: " + author);
+            System.out.println("출판사: " + publisher);
+            System.out.println("ISBN: " + isbn);
+
+            while (true) {
+                System.out.print("▶ 해당 내용이 맞습니까? (y/n) : ");
+                String check = br.readLine();
+
+                if (check.equalsIgnoreCase("y")) {
+                    books.addBook(book);
+                    System.out.println("책이 성공적으로 추가되었습니다.");
+                    System.out.println("------------------------------------");
+                    break;
+                } else if (check.equalsIgnoreCase("n")) {
+                    System.out.println("취소되었습니다.");
+                    System.out.println("------------------------------------");
+                    break;
+                } else {
+                    System.out.println("다시 입력해주세요.");
+                }
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 
     private void remove(BookList books) {
         try (Scanner scanner = new Scanner(System.in)) {
-            System.out.print("▶ 삭제하고 싶은 책의 ISBN을 입력하시오: ");
-            String isbn = scanner.nextLine();
+            while (true) {
+                System.out.print("▶ 삭제하고 싶은 책의 ISBN을 입력하시오: ");
+                String isbn = scanner.nextLine();
 
-            // ISBN으로 Book 객체 찾기
-            Book foundBook = null;
-            for (Book book : books.getBooks()) {
-                if (book.getIsbn().equals(isbn)) {
-                    foundBook = book;
-                    break;
-                }
-            }
-
-            if (foundBook != null) {
-                // ISBN이 일치하는 Book 객체를 찾았을 때의 처리
-                System.out.println("제목: " + foundBook.getTitle());
-                System.out.println("저자: " + foundBook.getAuthor());
-                System.out.println("출판사: " + foundBook.getPublisher());
-                System.out.println("ISBN: " + foundBook.getIsbn());
-
-                while (true) {
-                    System.out.print("▶ 삭제하시겠습니까? (y/n): ");
-                    String confirm = scanner.nextLine();
-
-                    switch (confirm) {
-                        case "y":
-                            books.removeBook(isbn);
-                            System.out.println("책이 삭제되었습니다.");
-                            System.out.println("------------------------------------");
-                            return;
-                        case "n":
-                            System.out.println("삭제가 취소되었습니다.");
-                            System.out.println("------------------------------------");
-                            return;
-                        default:
-                            System.out.println("다시 입력해주세요.");
-                            break;
-
+                // ISBN으로 Book 객체 찾기
+                Book foundBook = null;
+                for (Book book : books.getBooks()) {
+                    if (book.getIsbn().equals(isbn)) {
+                        foundBook = book;
+                        break;
                     }
                 }
 
-            } else {
-                // ISBN이 일치하는 Book 객체를 찾지 못했을 때의 처리
-                System.out.println("책을 찾을 수 없습니다.");
-                System.out.println("------------------------------------");
+                if (foundBook != null) {
+                    // ISBN이 일치하는 Book 객체를 찾았을 때의 처리
+                    System.out.println("제목: " + foundBook.getTitle());
+                    System.out.println("저자: " + foundBook.getAuthor());
+                    System.out.println("출판사: " + foundBook.getPublisher());
+                    System.out.println("ISBN: " + foundBook.getIsbn());
+
+                    while (true) {
+                        System.out.print("▶ 삭제하시겠습니까? (y/n): ");
+                        String confirm = scanner.nextLine();
+
+                        switch (confirm) {
+                            case "y":
+                                books.removeBook(isbn);
+                                System.out.println("책이 삭제되었습니다.");
+                                System.out.println("------------------------------------");
+                                return;
+                            case "n":
+                                System.out.println("삭제가 취소되었습니다.");
+                                System.out.println("------------------------------------");
+                                return;
+                            default:
+                                System.out.println("다시 입력해주세요.");
+                                break;
+
+                        }
+                    }
+
+                } else {
+                    // ISBN이 일치하는 Book 객체를 찾지 못했을 때의 처리
+                    System.out.println("책을 찾을 수 없습니다. 다시 입력해주세요.");
+                    continue;
+                }
             }
         }
-
     }
 
     private void ObserverAdd(BookList booklist) {
--- LMS/src/main/java/cseProject/BookManagement/BookList.java
+++ LMS/src/main/java/cseProject/BookManagement/BookList.java
@@ -8,12 +8,10 @@ package cseProject.BookManagement;
  *
  * @author Lenovo
  */
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
-import java.io.File;
 
 public class BookList implements Subject {
 
@@ -65,7 +63,7 @@ public class BookList implements Subject {
         String paths = System.getProperty("user.dir");
         File projectPath = new File(paths + "/DB/BookList.txt");
         
-        try (BufferedReader br = new BufferedReader(new FileReader(projectPath))) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(projectPath), StandardCharsets.UTF_8))) {
            String line;
 
             while ((line = br.readLine()) != null) {
