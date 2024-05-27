ea021a55eccfcec9a6af51f2f8f6d5e82db250ec
qkrwnsduda1 <qkrwnsduda1@naver.com>
2024-05-28 03:45:22 +0900

    기능별 커맨드 클래스를 Command 클래스의 내부 클래스로 변경

--- LMS/src/main/java/cseUsermanagementFunction/command/Command.java
+++ LMS/src/main/java/cseUsermanagementFunction/command/Command.java
@@ -4,10 +4,58 @@
  */
 package cseUsermanagementFunction.command;
 
+import cseUsermanagementFunction.UserManagementFunction;
+
 /**
  *
  * @author jasuj
  */
 public interface Command {
       void execute();
+      
+      
+      
+      
+      public class InspectUserListCommand implements Command {
+    private UserManagementFunction userManager = UserManagementFunction.getInstance();
+
+    public InspectUserListCommand() {
+    }
+
+    @Override
+    public void execute() {
+        userManager.InspectUserList();
+    }
+}
+      
+      
+      public class ModifyUserCommand implements Command {
+    private UserManagementFunction userManager = UserManagementFunction.getInstance();
+
+    public ModifyUserCommand() {
+    }
+
+    @Override
+    public void execute() {
+        userManager.ModifyUser();
+    }
+}
+      
+      
+      public class SearchUserCommand implements Command {
+    private UserManagementFunction userManager = UserManagementFunction.getInstance();
+
+    public SearchUserCommand() {
+    }
+
+    @Override
+    public void execute() {
+        userManager.SearchUserList();
+    }
+}
+      
+      
+      
+      
+      
 }
